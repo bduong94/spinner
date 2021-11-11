04809cf44cd7de5b3343ff249b7a5d6156f7d41a
Brian Duong <brianminhduong@gmail.com>
2021-11-11 04:07:35 +0000

    Use setTimeout to spin a line in a circle

---
+++ spinner1.js
@@ -0,0 +1,16 @@
+setTimeout(() => process.stdout.write('\r|   '), 20);
+setTimeout(() => process.stdout.write('\r/   '), 400);
+setTimeout(() => process.stdout.write('\r-   '), 600);
+setTimeout(() => process.stdout.write('\r\\   '), 800);
+setTimeout(() => process.stdout.write('\r-   '), 1000);
+setTimeout(() => process.stdout.write('\r|   '), 1200);
+setTimeout(() => process.stdout.write('\r/   '), 1400);
+setTimeout(() => process.stdout.write('\r-   '), 1600);
+setTimeout(() => process.stdout.write('\r\\   '), 1800);
+setTimeout(() => process.stdout.write('\r-   '), 2000);
+setTimeout(() => process.stdout.write('\r|   '), 2200);
+setTimeout(() => process.stdout.write('\r/   '), 2400);
+setTimeout(() => process.stdout.write('\r-   '), 2600);
+setTimeout(() => process.stdout.write('\r\\   '), 2800);
+setTimeout(() => process.stdout.write('\r-   '), 3000);
+setTimeout(() => process.stdout.write('\r|   \n'), 3200);
